0f76c2565fedb3248954cfdf2bf686808790ce45
hongchao <hongchao@klarna.com>
2014-08-16 00:26:23 +0200

    add home page

--- config/env/all.js
+++ config/env/all.js
@@ -1,45 +1,55 @@
 'use strict';
 
 module.exports = {
-	app: {
-		title: 'meanadex',
-		description: 'Meanadex is Monadex dressed up in the fancy cloak of MEAN stack',
-		keywords: 'tshirt, crowd sourcing, mean, lambda'
-	},
-	port: process.env.PORT || 3000,
-	templateEngine: 'swig',
-	sessionSecret: 'MEAN',
-	sessionCollection: 'sessions',
-	assets: {
-		lib: {
-			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-			],
-			js: [
-				'public/lib/angular/angular.js',
-				'public/lib/angular-resource/angular-resource.js', 
-				'public/lib/angular-cookies/angular-cookies.js', 
-				'public/lib/angular-animate/angular-animate.js', 
-				'public/lib/angular-touch/angular-touch.js', 
-				'public/lib/angular-sanitize/angular-sanitize.js', 
-				'public/lib/angular-ui-router/release/angular-ui-router.js',
-				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
-			]
-		},
-		css: [
-			'public/modules/**/css/*.css'
-		],
-		js: [
-			'public/config.js',
-			'public/application.js',
-			'public/modules/*/*.js',
-			'public/modules/*/*[!tests]*/*.js'
-		],
-		tests: [
-			'public/lib/angular-mocks/angular-mocks.js',
-			'public/modules/*/tests/*.js'
-		]
-	}
+  app: {
+    title: 'meanadex',
+    description: 'Meanadex is Monadex dressed up in the fancy cloak of MEAN stack',
+    keywords: 'tshirt, crowd sourcing, mean, lambda'
+  },
+  port: process.env.PORT || 3000,
+  templateEngine: 'swig',
+  sessionSecret: 'MEAN',
+  sessionCollection: 'sessions',
+  assets: {
+    lib: {
+      css: [
+        'public/lib/bootstrap/dist/css/bootstrap.css',
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css',
+        'public/lib/font-awesome/css/font-awesome.css'
+      ],
+      js: [
+        'public/lib/angular/angular.js',
+        'public/lib/angular-resource/angular-resource.js',
+        'public/lib/angular-cookies/angular-cookies.js',
+        'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-touch/angular-touch.js',
+        'public/lib/angular-sanitize/angular-sanitize.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.js',
+        'public/lib/angular-ui-utils/ui-utils.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'public/lib/jquery/jquery.js',
+        'public/lib/fabric/dist/fabric.js',
+        'public/lib/jquery-minicolors/jquery.minicolors.js',
+        'public/lib/bootstrap/dist/js/bootstrap.js',
+        'public/lib/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js',
+        'public/lib/textAngular/dist/textAngular-sanitize.min.js',
+        'public/lib/textAngular/dist/textAngular.min.js',
+        'public/lib/datejs/build/date.js'
+      ]
+    },
+    css: [
+      'public/modules/**/css/*.css'
+    ],
+    js: [
+      'public/config.js',
+      'public/application.js',
+      'public/modules/*/*.js',
+      'public/modules/*/*[!tests]*/*.js'
+    ],
+    tests: [
+      'public/lib/angular-mocks/angular-mocks.js',
+      'public/modules/*/tests/*.js'
+    ]
+  }
 };
