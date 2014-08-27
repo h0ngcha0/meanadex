5ebc330f7b21d4c1af2fa98039ce77efdea50b30
hongchao <hongchao@klarna.com>
2014-08-27 23:09:08 +0200

    bring the code that handls bootstrap modal back. need to investigate angular-modal

--- public/modules/designer/directives/md-tshirt-style-quality-panel.client.directive.js
+++ public/modules/designer/directives/md-tshirt-style-quality-panel.client.directive.js
@@ -1,12 +1,22 @@
 'use strict';
 
 angular.module('designer').directive('mdTshirtStyleQualityPanel', [
-  function() {
+  '$timeout', 'mdCanvasService',
+  function($timeout, mdCanvasService) {
     return {
       restrict: 'E',
       templateUrl: 'modules/designer/views/style-quality.client.view.html',
       link: function(scope, element, attrs) {
+        $timeout(function() {
+          element.find('#designNextStep').click(function(e) {
+            mdCanvasService.saveCanvas();
+            if(mdCanvasService.isEmptyCanvas()) {
+              console.log("empty");
+              element.find('#emptyCanvasModal').modal('show');
+              e.preventDefault();
+            }
+          })}, 0);
       }
-    };
-  }
+    }
+}
 ]);
