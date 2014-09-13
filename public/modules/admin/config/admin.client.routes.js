6efa350bac887cabbd0a2d46242763ae189b042d
hongchao <hongchao@klarna.com>
2014-09-13 16:58:21 +0200

    scaffolding for admin page

---
+++ public/modules/admin/config/admin.client.routes.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//Setting up route
+angular.module('admin').config(['$stateProvider',
+	function($stateProvider) {
+		// Admin state routing
+		$stateProvider.
+		state('admin', {
+			url: '/admin',
+			templateUrl: 'modules/admin/views/admin.client.view.html'
+		});
+	}
+]);
