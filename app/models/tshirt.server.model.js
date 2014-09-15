4c538c9908bc6d45b9a63752c7a2c221a76f9742
hongchao <hongchao@klarna.com>
2014-09-15 22:54:59 +0200

    add few pages for tshirt admin

--- app/models/tshirt.server.model.js
+++ app/models/tshirt.server.model.js
@@ -25,20 +25,57 @@ var TshirtSchema = new Schema({
     ref: 'User'
   },
   variants: {
-    type: [{name: {type: String},
-            description: {type: String},
-            baseCost: {type: Number},
-            unit: {type: {type: String,
-                          enum: ['RMB', 'SEK', 'USD']}},
-            colors: {type: [{title: {type: String}},
-                            {style: {type: String}}]}
-           }]
+    type: [
+      {
+        name: {
+          type: String,
+          required: 'variant name is required'
+        },
+        description: {
+          type: String,
+          required: 'variant description is required'
+        },
+        baseCost: {
+          type: Number,
+          required: 'variant baseCost is required'
+        },
+        unit: {
+          type: String,
+          default: 'SEK'
+        },
+        //colors: {
+        //  type: [
+        //    {
+        //      title: {
+        //        type: String,
+        //        required: 'color title is required'
+        //      }
+        //    },
+        //    {
+        //      style: {
+        //        type: String,
+        //        required: 'color style is required'
+        //      }
+        //    }
+        //  ],
+        //  required: 'variant colors is required',
+        //  default: []
+        //}
+        colors: {
+          type: String,
+          default: "black"
+        }
+      }
+    ],
+    required: 'At least have one tshirt variants'
   },
   frontImageUrl: {
-    type: String // URL
+    type: String, // URL
+    required: 'front image url required'
   },
   backImageUrl: {
-    type: String // URL
+    type: String, // URL
+    required: 'back image url required'
   }
 });
 
--- public/modules/tshirts/controllers/tshirts.client.controller.js
+++ public/modules/tshirts/controllers/tshirts.client.controller.js
@@ -3,14 +3,20 @@
 // Tshirts controller
 angular.module('tshirts').controller('TshirtsController', [
   '$scope', '$stateParams', '$location', 'Authentication', 'Tshirts',
-  function($scope, $stateParams, $location, Authentication, Tshirts ) {
+  '$filter', 'ngTableParams', '$timeout',
+  function($scope, $stateParams, $location, Authentication, Tshirts,
+           $filter, NgTableParams, $timeout) {
     $scope.authentication = Authentication;
+    $scope.variants = [];
 
     // Create new Tshirt
     $scope.create = function() {
       // Create new Tshirt object
       var tshirt = new Tshirts ({
-        name: this.name
+        name: this.name,
+        frontImageUrl: this.frontImage,
+        backImageUrl: this.backImage,
+        variants: this.variants
       });
 
       // Redirect after save
@@ -42,8 +48,8 @@ angular.module('tshirts').controller('TshirtsController', [
     };
 
     // Update existing Tshirt
-    $scope.update = function() {
-      var tshirt = $scope.tshirt ;
+    $scope.update = function(tshirt0) {
+      var tshirt = tshirt0 || $scope.tshirt;
 
       tshirt.$update(function() {
         $location.path('tshirts/' + tshirt._id);
@@ -63,5 +69,47 @@ angular.module('tshirts').controller('TshirtsController', [
         tshirtId: $stateParams.tshirtId
       });
     };
+
+    $scope.addTshirtVariant = function() {
+      console.log("niux");
+      $scope.variants.push({
+        name: "",
+        description: "",
+        baseCost: 0,
+        unit: ['SEK'],
+        colors: []
+      });
+    }
+
+    $scope.tshirtsTableParams = new NgTableParams(
+      {
+        page: 1,
+        count: 10
+      },
+      {
+        total: 0,
+        getData: function($defer, params) {
+          $timeout(function() {
+            var orderedData = params.filter() ?
+              $filter('filter')($scope.tshirts, params.filter()) : $scope.tshirts;
+
+            $scope.presentedTshirts = orderedData;
+
+            params.total($scope.presentedTshirts.length);
+            $defer.resolve($scope.presentedTshirts);
+          }, 500);
+        }
+      }
+    );
+
+    $scope.onSave = function(tshirt) {
+      tshirt.$edit = false;
+      $scope.update(tshirt);
+    };
+
+    $scope.onEdit = function(tshirt) {
+      tshirt.$edit = true;
+    };
+
   }
 ]);
