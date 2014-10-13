0b5fabbea04fd390ebbe1c05d6e360ef080f11ce
hongchao <hongchao@klarna.com>
2014-10-13 23:50:13 +0200

    failed attempt, user not logged in

--- populate-test-data.js
+++ populate-test-data.js
@@ -3,14 +3,68 @@
 /**
  * Module dependencies.
  */
-var server = require('./server');
+var request = require('request'),
+    async = require('async');
 
 module.exports = function(grunt) {
   grunt.task.registerTask(
     'populateTestData',
     'Task that populats test data into the database.',
     function() {
-      console.log('populating test data..');
+      var done = this.async();
+
+      var createAdminUser = function(callback) {
+        request.post(
+          'http://localhost:3000/auth/signup',
+          {
+            form: {
+              firstName: 'admin',
+              lastName: 'gustav',
+              email: 'admin@meanadex.com',
+              username: 'admin',
+              password: 'password'
+            }
+          },
+          function(error, response, body) {
+            var user = body;
+            callback(error, user);
+          }
+        );
+      };
+
+      var createFrontImages = function(user, callback) {
+        request.post(
+          'http://localhost:3000/images',
+          {
+            auth: {
+              'user': 'admin',
+              'pass': 'password',
+              'sendImmediately': false
+            },
+            form: {
+              url: 'niux',
+              user: user
+            }
+          },
+          function(error, response, body) {
+            callback(error, body);
+          }
+        );
+      };
+
+      var resultCallback = function(err, results) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log(results);
+          console.log('success!')
+        }
+      };
+
+      async.waterfall([
+        createAdminUser,
+        createFrontImages
+      ], resultCallback);
     }
   );
 };
