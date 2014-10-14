aac4c089089b689aa26ff75e98819d5574ca22e4
hongchao <hongchao@klarna.com>
2014-10-15 00:20:46 +0200

    upload tshirt

--- populate-test-data.js
+++ populate-test-data.js
@@ -27,7 +27,8 @@ module.exports = function(grunt) {
             }
           },
           function(error, response, body) {
-            var user = body;
+            var user = JSON.parse(body);
+            console.log('Created admin user: ' + user.displayName);
             callback(error, user);
           }
         );
@@ -44,23 +45,82 @@ module.exports = function(grunt) {
             }
           },
           function(error, response, body) {
-            callback(error, body);
+            var img = JSON.parse(body),
+                frontImgId = img._id;
+            console.log('Uploaded front image: ' + img.url);
+            callback(error, frontImgId);
           }
         );
       };
+
+      var createBackImages = function(frontImgId, callback) {
+        request.post(
+          'http://localhost:3000/images',
+          {
+            formData: {
+              file: fs.createReadStream(
+                __dirname + '/public/modules/designer/img/canvas/crew_back.png'
+              )
+            }
+          },
+          function(error, response, body) {
+            var img = JSON.parse(body),
+                backImgId = img._id;
+            console.log('Uploaded back image: ' + img.url);
+            callback(error, frontImgId, backImgId);
+          }
+        );
+      };
+
+      var createTshirt = function(frontImgId, backImgId, callback) {
+        request.post(
+          'http://localhost:3000/tshirts',
+          {
+            form: {
+              name: 'Meanadex signature Tee',
+              frontImage: frontImgId,
+              backImage: backImgId,
+              variants: [
+                {
+                  name: 'All cutton',
+                  description: 'Good value',
+                  baseCost: 50,
+                  unit: 'SEK',
+                  colors: ['white', 'red', 'green']
+                },
+                {
+                  name: 'Premium',
+                  description: 'Good quality',
+                  baseCost: 70,
+                  unit: 'SEK',
+                  colors: ['green', 'black', 'yellow']
+                }
+              ]
+            }
+          },
+          function(error, response, body) {
+            var tshirt = JSON.parse(body);
+            console.log('Tshirt: ' + tshirt.name + ' is uploaded');
+            callback(error, tshirt);
+          }
+        );
+      }
 
       var resultCallback = function(err, results) {
         if(err) {
           done(false);
         } else {
-          console.log('success!')
+          console.log('-----');
+          console.log('Done!')
           done()
         }
       };
 
       async.waterfall([
         createAdminUser,
-        createFrontImages
+        createFrontImages,
+        createBackImages,
+        createTshirt
       ], resultCallback);
     }
   );
