488b141a64f07056a805757314cea1f5c70210b6
hongchao <hongchao@klarna.com>
2014-12-11 00:25:08 +0100

    style menu bar, put sign in/up back

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('core').controller('HeaderController', [
-  '$scope', 'Authentication', 'Menus',
-  function($scope, Authentication, Menus) {
+  '$scope', 'Authentication', 'Menus', '$location',
+  function($scope, Authentication, Menus, $location) {
     $scope.authentication = Authentication;
     $scope.isCollapsed = false;
     $scope.menu = Menus.getMenu('topbar');
