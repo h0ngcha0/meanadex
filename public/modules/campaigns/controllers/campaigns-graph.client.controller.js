28d07f433bfd4b4ee2a630015eb5ff7bc6570c73
hongchao <hongchao@klarna.com>
2014-12-16 23:19:57 +0100

    introduce CampaignsGraphController

---
+++ public/modules/campaigns/controllers/campaigns-graph.client.controller.js
@@ -0,0 +1,65 @@
+'use strict';
+
+// Campaigns controller
+angular.module('campaigns').controller('CampaignsGraphController', [
+  '$scope', 'Authentication', 'Campaigns',
+  function($scope, Authentication, Campaigns) {
+    $scope.authentication = Authentication;
+
+    $scope.findBetween = function(start, end) {
+      $scope.campaigns = Campaigns.query(
+        {
+          startDate: start,
+          endDate: end
+        }
+      );
+    };
+
+    // Fake data for now
+    $scope.campaignGraphData = [
+      {
+        'key': 'Series 1',
+        values: [
+          [0, 10],
+          [1, 20],
+          [2, 90],
+          [3, 20],
+          [4, 40],
+          [5, 10],
+          [9, 10],
+          [100, 10]
+        ]
+      }
+    ];
+
+    // date picker related
+    $scope.today = Date.today();
+    $scope.weekAgo = Date.today().addDays(-7);
+    $scope.fromDate = $scope.weekAgo;
+    $scope.toDate = $scope.today;
+
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
+
+    $scope.date = {
+      fromOpened: false,
+      toOpened: false
+    };
+
+    $scope.openFromDate = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope.date.fromOpened = true;
+    };
+
+    $scope.openToDate = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope.date.toOpened = true;
+    };
+  }
+]);
--- public/modules/dashboard/controllers/dashboard.client.controllers.js
+++ public/modules/dashboard/controllers/dashboard.client.controllers.js
@@ -43,53 +43,5 @@ angular.module('dashboard').controller('DashboardController', [
     window.onresize = function() {
         $scope.$apply();
     };
-
-
-    // Fake data for now
-    $scope.campaignGraphData = [
-      {
-        'key': 'Series 1',
-        values: [
-          [0, 10],
-          [1, 20],
-          [2, 90],
-          [3, 20],
-          [4, 40],
-          [5, 10],
-          [9, 10],
-          [100, 10]
-        ]
-      }
-    ];
-
-    // date picker related
-    $scope.today = Date.today();
-    $scope.weekAgo = Date.today().addDays(-7);
-    $scope.fromDate = $scope.weekAgo;
-    $scope.toDate = $scope.today;
-
-    $scope.dateOptions = {
-      formatYear: 'yy',
-      startingDay: 1
-    };
-
-    $scope.date = {
-      fromOpened: false,
-      toOpened: false
-    };
-
-    $scope.openFromDate = function($event) {
-      $event.preventDefault();
-      $event.stopPropagation();
-
-      $scope.date.fromOpened = true;
-    };
-
-    $scope.openToDate = function($event) {
-      $event.preventDefault();
-      $event.stopPropagation();
-
-      $scope.date.toOpened = true;
-    };
   }
 ]);
