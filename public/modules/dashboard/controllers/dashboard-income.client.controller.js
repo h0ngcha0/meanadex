2b8a9afd6072b2d016cacfeda0959ff280a28068
hongchao <hongchao@klarna.com>
2014-12-27 12:25:23 +0100

    add income graph

---
+++ public/modules/dashboard/controllers/dashboard-income.client.controller.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/* global moment */
+/* global d3 */
+
+// Campaigns controller
+angular.module('dashboard').controller('DashboardIncomeGraphController', [
+  '$scope', 'Dashboard',
+  function($scope, Dashboard) {
+    $scope.loadIncomeData = function(fromDate, toDate, callback) {
+      Dashboard.incomeCreated.query(
+        {
+          startDate: fromDate,
+          endDate: toDate,
+          offset: new Date().getTimezoneOffset()
+        },
+        function(data) {
+          console.log('income data:');
+          console.log(data);
+          callback(null, data);
+        },
+        function(err) {
+          callback(err);
+        }
+      );
+    };
+  }
+]);
