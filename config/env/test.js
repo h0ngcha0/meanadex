221aac98988a5dcce17b7a314c29b32c24f69f26
Jacky Hu <hudayou@hotmail.com>
2015-01-18 22:00:41 +0800

    Assets should not be placed in test enviroment

--- config/env/test.js
+++ config/env/test.js
@@ -8,61 +8,6 @@ module.exports = {
   app: {
     title: 'meanadex - Test Environment'
   },
-  assets: {
-    lib: {
-      css: [
-        'public/lib/bootstrap/dist/css/bootstrap.min.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-        'public/lib/font-awesome/css/font-awesome.min.css',
-        'public/lib/angular-bootstrap-colorpicker.min.css/colorpicker.min.css',
-        'public/lib/ng-table/ng-table.min.css',
-        'public/lib/nvd3/nv.d3.min.css',
-        'public/lib/angular-rangeslider/angular.rangeSlider.css',
-        'public/lib/ng-tags-input/ng-tags-input.min.css',
-        'public/lib/angular-motion/dist/angular-motion.min.css'
-      ],
-      js: [
-        'public/lib/jquery/jquery.min.js',
-        'public/lib/lodash/dist/lodash.underscore.min.js',
-        'public/lib/async/lib/async.js',
-        'https://checkout.stripe.com/checkout.js',
-        'public/lib/angular/angular.min.js',
-        'public/lib/angular-resource/angular-resource.min.js',
-        'public/lib/angular-cookies/angular-cookies.min.js',
-        'public/lib/angular-animate/angular-animate.min.js',
-        'public/lib/angular-strap/dist/angular-strap.min.js',
-        'public/lib/angular-strap/dist/angular-strap.tpl.min.js',
-        'public/lib/angular-touch/angular-touch.min.js',
-        'public/lib/angular-sanitize/angular-sanitize.min.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/angular-ui-utils/ui-utils.min.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/fabric/dist/fabric.min.js',
-        'public/lib/bootstrap/dist/js/bootstrap.min.js',
-        'public/lib/textAngular/dist/textAngular-sanitize.min.js',
-        'public/lib/textAngular/dist/textAngular.min.js',
-        'public/lib/moment/min/moment.min.js',
-        'public/lib/angular-bootstrap-colorpicker/js/bootstrap-colorpicker-module.js',
-        'public/lib/angular-rangeslider/angular.rangeSlider.js',
-        'public/lib/angular-local-storage/angular-local-storage.min.js',
-        'public/lib/ng-table/ng-table.min.js',
-        'public/lib/angular-data/dist/angular-data.min.js',
-        'public/lib/angular-timer/dist/angular-timer.min.js',
-        'public/lib/angular-file-upload/angular-file-upload.min.js',
-        'public/lib/d3/d3.min.js',
-        'public/lib/nvd3/nv.d3.min.js',
-        'public/lib/angularjs-nvd3-directives/dist/angularjs-nvd3-directives.min.js',
-        'public/lib/bootstrap-autohidingnavbar/dist/jquery.bootstrap-autohidingnavbar.min.js',
-        'public/lib/ng-tags-input/ng-tags-input.min.js'
-      ]
-    },
-    css: 'public/dist/application.min.css',
-    js: 'public/dist/application.min.js',
-    tests: [
-      'public/lib/angular-mocks/angular-mocks.js',
-      'public/modules/*/tests/*.js'
-    ]
-  },
   stripe: {
     clientID: process.env.STRIPE_ID || 'ca_53emHeHCgJWdAOPGsIp4uIdFCCEXkdal',
     clientSecret: process.env.STRIPE_SECRET || 'sk_test_POGF3C0J4jmm8rFZNGwLrLaH',
