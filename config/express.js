65bb3b844204e9172e7e095ab4ec9e5a82fa843d
Jacky Hu <hudayou@hotmail.com>
2015-01-18 18:58:38 +0800

    Do not render 404 and 500 errors

--- config/express.js
+++ config/express.js
@@ -114,14 +114,14 @@ module.exports = function(db) {
     console.error(err.stack);
 
     // Error page
-    res.status(500).render('500', {
+    res.status(500).send({
       error: err.stack
     });
   });
 
   // Assume 404 since no middleware responded
   app.use(function(req, res) {
-    res.status(404).render('404', {
+    res.status(404).send({
       url: req.originalUrl,
       error: 'Not Found'
     });
