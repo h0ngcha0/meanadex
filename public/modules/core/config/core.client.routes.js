d7c68386d308845cc3962d035251d76ed33af48f
Jacky Hu <hudayou@hotmail.com>
2015-01-28 22:55:51 +0800

    Listen to $stateChangeError on $rootScope

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -13,4 +13,13 @@ angular.module('core').config([
         url: '/landing'
       });
   }
-]);
+]).run(['$log', '$rootScope', '$state', function ($log, $rootScope, $state) {
+
+  // Listen to changes on the root scope. If it's an error in the state
+  // changes (i.e. a 404) take the user back to the index.
+  $rootScope.$on('$stateChangeError',
+    function () {
+      $state.go('index');
+    }
+  );
+}]);
