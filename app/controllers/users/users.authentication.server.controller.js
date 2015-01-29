6bdf38594116ba0ad31132c6e1213bd5631193a1
Jacky Hu <hudayou@hotmail.com>
2015-01-29 22:55:50 +0800

    Distinguish between authroize and connect

--- app/controllers/users/users.authentication.server.controller.js
+++ app/controllers/users/users.authentication.server.controller.js
@@ -41,70 +41,105 @@ exports.signup = function(req, res, next) {
  * Helper function to save or update a OAuth user profile
  */
 exports.saveOAuthUserProfile = function(req, providerUserProfile, callback) {
-  // Define a search query fields
-  var searchMainProviderIdentifierField = 'providerData.id';
-  var searchAdditionalProviderIdentifierField = 'additionalProvidersData.' + providerUserProfile.service + '.id';
-
-  // Define main provider search query
-  var mainProviderSearchQuery = {};
-  mainProviderSearchQuery.provider = providerUserProfile.service;
-  mainProviderSearchQuery[searchMainProviderIdentifierField] = providerUserProfile.id;
-
-  // Define additional provider search query
-  var additionalProviderSearchQuery = {};
-  additionalProviderSearchQuery[searchAdditionalProviderIdentifierField] = providerUserProfile.id;
-
-  // Define a search query to find existing user with current provider profile
-  var searchQuery = {
-    $or: [mainProviderSearchQuery, additionalProviderSearchQuery]
-  };
-
-  User.findOne(searchQuery, function(err, user) {
-    if (err) {
-      return callback(err);
-    } else {
+  var state = req.query.state;
+  // Decode state (possibly by JWT)
+  // Try find user by decoded state
+  User.findOne(
+    {
+      _id: state
+    },
+    function(err, user) {
+      if (err) {
+        return callback(err);
+      }
+      // Authroize a user by an oauth provider
       if (!user) {
-        var username = providerUserProfile.data.email;
-        User.findOne({username: username}, function(err, user) {
+        // Define a search query fields
+        var searchMainProviderIdentifierField = 'providerData.id';
+        var searchAdditionalProviderIdentifierField = 'additionalProvidersData.' + providerUserProfile.service + '.id';
+
+        // Define main provider search query
+        var mainProviderSearchQuery = {};
+        mainProviderSearchQuery.provider = providerUserProfile.service;
+        mainProviderSearchQuery[searchMainProviderIdentifierField] = providerUserProfile.id;
+
+        // Define additional provider search query
+        var additionalProviderSearchQuery = {};
+        additionalProviderSearchQuery[searchAdditionalProviderIdentifierField] = providerUserProfile.id;
+
+        // Define a search query to find existing user with current provider profile
+        var searchQuery = {
+          $or: [mainProviderSearchQuery, additionalProviderSearchQuery]
+        };
+
+        User.findOne(searchQuery, function(err, user) {
           if (err) {
             return callback(err);
           } else {
             if (!user) {
-              user = new User({
-                username: username,
-                provider: providerUserProfile.service,
-                providerData: providerUserProfile
-              });
+              var username = providerUserProfile.data.email;
+              User.findOne({username: username}, function(err, user) {
+                if (err) {
+                  return callback(err);
+                } else {
+                  if (!user) {
+                    user = new User({
+                      username: username,
+                      provider: providerUserProfile.service,
+                      providerData: providerUserProfile
+                    });
 
-              // And save the user
-              return user.save(function(err, user) {
-                callback(err, user);
+                    // And save the user
+                    return user.save(function(err, user) {
+                      callback(err, user);
+                    });
+                  } else {
+                    // Check if user exists, is not signed in using this provider, and doesn't have that provider data already configured
+                    if (user.provider !== providerUserProfile.service && (!user.additionalProvidersData || !user.additionalProvidersData[providerUserProfile.service])) {
+                      // Add the provider data to the additional provider data field
+                      if (!user.additionalProvidersData) user.additionalProvidersData = {};
+                      user.additionalProvidersData[providerUserProfile.service] = providerUserProfile;
+
+                      // Then tell mongoose that we've updated the additionalProvidersData field
+                      user.markModified('additionalProvidersData');
+
+                      // And save the user
+                      return user.save(function(err, user) {
+                        callback(err, user);
+                      });
+                    } else {
+                      return callback(err, user);
+                    }
+                  }
+                }
               });
             } else {
-              // Check if user exists, is not signed in using this provider, and doesn't have that provider data already configured
-              if (user.provider !== providerUserProfile.service && (!user.additionalProvidersData || !user.additionalProvidersData[providerUserProfile.service])) {
-                // Add the provider data to the additional provider data field
-                if (!user.additionalProvidersData) user.additionalProvidersData = {};
-                user.additionalProvidersData[providerUserProfile.service] = providerUserProfile;
-
-                // Then tell mongoose that we've updated the additionalProvidersData field
-                user.markModified('additionalProvidersData');
-
-                // And save the user
-                return user.save(function(err, user) {
-                  callback(err, user);
-                });
-              } else {
-                return callback(err, user);
-              }
+              return callback(err, user);
             }
           }
         });
-      } else {
-        return callback(err, user);
+      }
+      // Connect to an oauth provider
+      else {
+        // Check if user exists, is not signed in using this provider, and doesn't have that provider data already configured
+        if (user.provider !== providerUserProfile.service && (!user.additionalProvidersData || !user.additionalProvidersData[providerUserProfile.service])) {
+          // Add the provider data to the additional provider data field
+          if (!user.additionalProvidersData) user.additionalProvidersData = {};
+          user.additionalProvidersData[providerUserProfile.service] = providerUserProfile;
+
+          // Then tell mongoose that we've updated the additionalProvidersData field
+          user.markModified('additionalProvidersData');
+
+          // And save the user
+          return user.save(function(err, user) {
+            callback(err, user);
+          });
+        } else {
+          return callback(err, user);
+        }
       }
     }
-  });
+  );
 };
 
 /**
