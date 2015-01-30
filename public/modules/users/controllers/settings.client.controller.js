be050de046ce79d27ea4ea8eb8fdf71641ef7292
Jacky Hu <hudayou@hotmail.com>
2015-01-30 20:49:32 +0800

    Bring back connect to stripe, facebook and google

--- public/modules/users/controllers/settings.client.controller.js
+++ public/modules/users/controllers/settings.client.controller.js
@@ -5,6 +5,40 @@ angular.module('users').controller('SettingsController', [
   function($scope, $http, $location, Users, currentUser) {
     $scope.user = currentUser;
 
+    // Check if there are additional accounts
+    $scope.hasConnectedAdditionalSocialAccounts = function(provider) {
+      for (var i in $scope.user.additionalProvidersData) {
+        return true;
+      }
+
+      return false;
+    };
+
+    // Check if provider is already in use with current user
+    $scope.isConnectedSocialAccount = function(provider) {
+      return $scope.user.provider ===
+      provider ||
+      ($scope.user.additionalProvidersData &&
+        $scope.user.additionalProvidersData[provider]);
+    };
+
+    // Remove a user social account
+    $scope.removeUserSocialAccount = function(provider) {
+      $scope.success = $scope.error = null;
+
+      $http.delete('/users/accounts', {
+        params: {
+          provider: provider
+        }
+      }).success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        $scope.user = response;
+      }).error(function(response) {
+        $scope.error = response.message;
+      });
+    };
+
     // Update a user profile
     $scope.updateUserProfile = function() {
       var user = new Users($scope.user);
