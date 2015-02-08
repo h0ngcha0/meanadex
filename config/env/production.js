c078f9d66ff27cba0a4d3b9ba06188a0cc034282
hongchao <hongchao@klarna.com>
2015-02-08 02:40:32 +0100

    use mootee-images bucket

--- config/env/production.js
+++ config/env/production.js
@@ -35,7 +35,7 @@ module.exports = {
         accessKeyId: 'AKIAIXDR74RCRQ2VSFLA',
         secretAccessKey: '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',
         region: 'us-west-2',
-        bucketName: 'mootee-images-test'
+        bucketName: 'mootee-images'
       }
     }
   },
