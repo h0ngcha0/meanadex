e4106193508f72f05f3ff45103c53e14617633a1
hongchao <hongchao@klarna.com>
2015-02-08 01:09:30 +0100

    close navbar toggle menu after clicking item

--- src/modules/core/services/landing.js
+++ src/modules/core/services/landing.js
@@ -31,6 +31,13 @@ $(document).ready(function () {
     $('html').getNiceScroll().resize();
   });
 
+
+  $(document).on('click','.navbar-collapse.in',function(e) {
+    if($(e.target).is('a') && $(e.target).attr('class') !== 'dropdown-toggle') {
+      $(this).collapse('hide');
+    }
+  });
+
   /********************************************************************
    3) Slideshow Heigth
    *******************/
