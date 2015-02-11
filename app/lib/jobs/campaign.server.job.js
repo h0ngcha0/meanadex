1c0ed6ad25c40ee18e4708892deaedfacc288df9
hongchao <lbs.lhc@gmail.com>
2015-02-11 12:00:47 +0100

    charge using designer access token if possible, otherwise charge on behalf of them first

--- app/lib/jobs/campaign.server.job.js
+++ app/lib/jobs/campaign.server.job.js
@@ -10,7 +10,7 @@ var mongoose = require('mongoose'),
 module.exports = function(agenda, config) {
   var stripe = require('stripe')(config.stripe.clientSecret);
 
-  var listAllOrders = function(campaign) {
+  function listAllOrders(campaign) {
     return function(callback) {
       var query = Order
                     .find({'campaign': campaign._id})
@@ -25,45 +25,91 @@ module.exports = function(agenda, config) {
         callback(err, campaign, campaignOrders);
       });
     };
-  };
+  }
 
-  var maybeChargeOrder = function(order, chargeFlag, accessToken) {
+  function createChargeObj(order, campaign, accessToken) {
+    var customerId = order.payment.customerId;
+
+    if(!accessToken) {
+      // If no access token for designer, we will charge
+      // customer the full amount, will have to manually
+      // transfer money to the designer in some way
+      // `amount` field says how much money will go to our
+      // account.
+      return {
+        customer: customerId,
+        amount: order.amount * 100,
+        currency: order.currency,
+        description: order.description
+      };
+    } else {
+      // If we have access token for designer, we will charge
+      // customer the same amount, but devided into two parts
+      // now the `amount` field says how much money will go to
+      // designer's account and `application_fee` field says
+      // how much money will go to our account. No transfer is
+      // needed.
+
+      var tshirtPrice = campaign.tshirt.baseCost;
+      var quantity = order.quantity;
+
+      var tshirtCost = tshirtPrice * quantity;
+      var designerProfit = order.amount - tshirtCost;
+      return {
+        customer: customerId,
+        amount: designerProfit * 100,
+        currency: order.currency,
+        description: order.description,
+        application_fee: tshirtCost
+      };
+    }
+  }
+
+  function charge(accessToken, chargeObj, callback) {
+    // If we have designer's access token, use it
+    // otherwise we collect all the money first.
+    if(accessToken) {
+      stripe.charges.create(
+        chargeObj,
+        accessToken,
+        callback
+      );
+    } else {
+      stripe.charges.create(
+        chargeObj,
+        callback
+      );
+    }
+  }
+
+  function maybeChargeOrder(order, chargeFlag, campaign, accessToken) {
     return function(callback) {
-      // making the charge
       var customerId = order.payment.customerId;
 
       // chargeFlag determines if charge or not.
       if(chargeFlag) {
-        // FIXME: need to charge on behalf of the campaign owner
-        stripe.charges.create(
-          {
-            customer: customerId,
-            amount: order.amount * 100,
-            currency: order.currency,
-            description: order.description
-          },
-          function(err, charge) {
-            if(err) {
-              logger.error(
-                'error charging order: ' + order._id + '; customer id: ' + customerId
-              );
-            } else {
-              logger.info(
-                'order: ' + order._id + ' with customer id: ' + customerId + ' charged.'
-              );
-            }
-
-            callback(err, customerId);
+        var chargeObj = createChargeObj(order, campaign, accessToken);
+        charge(accessToken, chargeObj, function(err, charge) {
+          if(err) {
+            logger.error(
+              'error charging order: ' + order._id + '; customer id: ' + customerId
+            );
+          } else {
+            logger.info(
+              'order: ' + order._id + ' with customer id: ' + customerId + ' charged.'
+            );
           }
-        );
+
+          callback(err, customerId);
+        });
       } else {
         // if not charge, proceed.
         callback(undefined, customerId);
       }
     };
-  };
+  }
 
-  var deleteCustomer = function(err, customerId) {
+  function deleteCustomer(err, customerId) {
     stripe.customers.del(
       customerId,
       function(err, confirmation) {
@@ -74,7 +120,7 @@ module.exports = function(agenda, config) {
         }
       }
     );
-  };
+  }
 
   var getUserStripeToken = function(campaign, campaignOrders, callback) {
     utils.fetchStripeAccessToken(campaign.user, function(err, accessToken) {
@@ -84,20 +130,23 @@ module.exports = function(agenda, config) {
     });
   };
 
-  var maybeChargeOrders = function(campaign, campaignOrders, accessToken, callback) {
+  function maybeChargeOrders(campaign, campaignOrders, accessToken, callback) {
     var numOrders = campaignOrders.length;
 
     var goalReached = numOrders >= campaign.goal ? true : false;
 
+    // TODO: Remind user if accessToken is not set, especially when goal is
+    //       reached or almost reached.
+
     _.forEach(campaignOrders, function(order) {
       async.waterfall([
-        maybeChargeOrder(order, goalReached, accessToken)
+        maybeChargeOrder(order, goalReached, campaign, accessToken)
       ], deleteCustomer);
     });
 
     // always go through
     callback(undefined, campaign, goalReached);
-  };
+  }
 
   var changeCampaignState = function(campaign, goalReached, callback) {
     campaign.state = goalReached ? 'tipped' : 'expired';
